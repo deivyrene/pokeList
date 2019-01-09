a4d5f37b70c4f2b37afe79f649f443d1b5af0d85
deivyrene <deivyhernandezperez@gmail.com>
2019-01-09 10:28:33 -0300

    Cambio vista

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -11,6 +11,19 @@ class Pokemon
     def self.detail id
         response = get("/#{id}")
         details = JSON.parse(response.body)
-        
+
+        #height = details["height"]
+        #name = details["name"]
+        #weight = details["weight"]
+        #national_id = details["id"]
+        #moves = details["moves"]
+
+        #array = ['height' => "#{height}", 'name' => name, 'weight' => "#{weight}",'id' => "#{national_id}", 'moves' => "#{moves}"]  
+
+        #data = array.to_json
+
+       # parsed_data = JSON.parse(data)
+
+        #puts parsed_data[0]["moves"]
     end
 end
